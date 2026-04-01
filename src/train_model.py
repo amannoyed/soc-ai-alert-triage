52e79b98f041e87867f7d3dad883a76702cf387b
Aman Ali <118153081+amannoyed@users.noreply.github.com>
2026-04-02 03:13:23 +0530

    Add files via upload

--- src/train_model.py
+++ src/train_model.py
@@ -1,26 +1,32 @@
+import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 import joblib
 import os
-from preprocess import load_data
 
-df = load_data()
+# 🔥 FIXED PATHS
+base_path = os.path.dirname(os.path.dirname(__file__))
 
+data_path = os.path.join(base_path, "data", "sample_logs.csv")
+model_dir = os.path.join(base_path, "model")
+model_path = os.path.join(model_dir, "model.pkl")
+
+# Load data
+df = pd.read_csv(data_path)
+
+# Features & labels
 X = df.drop("label", axis=1)
 y = df["label"]
 
+# Train/test split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
+# Model
 model = RandomForestClassifier()
 model.fit(X_train, y_train)
 
-# 🔥 FIXED PATH
-base_path = os.path.dirname(os.path.dirname(__file__))
-model_dir = os.path.join(base_path, "model")
-
+# Save model
 os.makedirs(model_dir, exist_ok=True)
-
-model_path = os.path.join(model_dir, "model.pkl")
 joblib.dump(model, model_path)
 
-print("Model trained and saved!")
+print("Model trained and saved successfully!")
